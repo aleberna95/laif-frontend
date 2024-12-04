ae430f49228375cbe6a07558de15488790ce32bf
Alessio Bernardini <51231763+aleberna95@users.noreply.github.com>
2024-12-04 23:51:38 +0100

    fix navbar

--- src/components/layout/BottomNavbar.vue
+++ src/components/layout/BottomNavbar.vue
@@ -1,52 +1,58 @@
 <template>
   <span class="fixed bottom-0 left-0 right-0 w-full flex justify-center">
     <nav
-      class="bg-white border-t-4 border-gray-300 shadow-lg flex justify-between items-center px-6 py-2 lg:w-4/12 w-full pb-10">
+      class="bg-white border-t-4 border-gray-300 shadow-lg flex justify-between items-center px-6 py-2 lg:w-4/12 w-full pb-12">
       <!-- Link Home -->
       <router-link to="/" class="flex flex-col items-center text-gray-600">
-        <div class="hover:shadow-xl p-1 rounded">
-          <img class="inline-block size-12 h-4 w-4" :src="homeImage" alt="" />
+        <div class="hover:shadow-xl hover:rounded-xl p-1 rounded">
+          <img class="inline-block size-12 h-6 w-6" :src="homeImage" alt="" />
         </div>
-<!--         <span class="text-xs">Home</span>
- -->      </router-link>
+        <!--         <span class="text-xs">Home</span>
+ -->
+      </router-link>
 
       <!-- Icona Lista Operazioni -->
       <router-link to="/operations" class="flex flex-col items-center text-gray-600">
-        <div class="hover:shadow-xl p-1 rounded">
-          <img class="inline-block size-12 h-4 w-4" :src="filesImage" alt="" />
+        <div class="hover:shadow-xl hover:rounded-xl p-1 rounded">
+          <img class="inline-block size-12 h-6 w-6" :src="filesImage" alt="" />
         </div>
-<!--         <span class="text-xs">Operazioni</span>
- -->      </router-link>
+        <!--         <span class="text-xs">Operazioni</span>
+ -->
+      </router-link>
 
       <!-- Icona + al centro -->
       <router-link to="/addOperation" class="flex flex-col items-center text-gray-600">
-        <div class="hover:shadow-xl p-1 rounded">
-          <img class="inline-block size-12 h-6 w-6" :src="addImage" alt="" />
+        <div class="hover:shadow-xl hover:rounded-xl p-1 rounded">
+          <img class="inline-block size-12 h-8 w-8" :src="addImage" alt="" />
         </div>
-<!--         <span class="text-xs">Aggiungi</span>
- -->      </router-link>
+        <!--         <span class="text-xs">Aggiungi</span>
+ -->
+      </router-link>
 
       <!-- Icona tips -->
       <router-link to="/tips" class="flex flex-col items-center text-gray-600">
-        <div class="hover:shadow-xl p-1 rounded">
-          <img class="inline-block size-12 h-4 w-4" :src="piggyBankImage" alt="" />
+        <div class="hover:shadow-xl hover:rounded-xl p-1 rounded">
+          <img class="inline-block size-12 h-6 w-6" :src="piggyBankImage" alt="" />
         </div>
-<!--         <span class="text-xs">Consigli</span>
- -->      </router-link>
+        <!--         <span class="text-xs">Consigli</span>
+ -->
+      </router-link>
 
       <!-- Link Login/Utente -->
       <router-link v-show="!isAuthenticated" to="/login" class="flex flex-col items-center text-gray-600">
-        <div class="hover:shadow-xl p-1 rounded">
-          <img class="inline-block size-12 rounded-full h-4 w-4" :src="loginImage" alt="" />
+        <div class="hover:shadow-xl hover:rounded-xl p-1 rounded">
+          <img class="inline-block size-12 h-6 w-6" :src="loginImage" alt="" />
         </div>
-<!--         <span class="text-xs">Login</span>
- -->      </router-link>
+        <!--         <span class="text-xs">Login</span>
+ -->
+      </router-link>
       <router-link v-show="isAuthenticated" to="/user" class="flex flex-col items-center text-gray-600">
-        <div class="hover:shadow-xl p-1 rounded">
-          <img class="inline-block size-12 rounded-full h-4 w-4" :src="userImage" alt="" />
+        <div class="hover:shadow-xl hover:rounded-xl p-1 rounded">
+          <img class="inline-block size-12 h-6 w-6" :src="userImage" alt="" />
         </div>
-<!--         <span class="text-xs">Utente</span>
- -->      </router-link>
+        <!--         <span class="text-xs">Utente</span>
+ -->
+      </router-link>
     </nav>
   </span>
 </template>
